0303a64241b7827e3a3332bd108e2ac7af3f426f
AyushGothi123 <72344991+AyushGothi123@users.noreply.github.com>
2021-02-17 10:30:44 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,53 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    getScore1(){
+        var player1ScoreRef = database.ref('player1Score');
+        player1ScoreRef.on("value",(data)=>{
+            player1Score = data.val();
+        })
+    }
+
+    getScore2(){
+        var player2ScoreRef = database.ref('player2Score');
+        player2ScoreRef.on("value",(data)=>{
+            player2Score = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
